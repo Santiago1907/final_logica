5ab4242da930aefc126db8b7951dfd3e5dca9d0e
Santiago <santiago.martinezri@upb.edu.co>
2020-10-22 19:14:29 -0500

    Programa terminado

--- Programa.java
+++ Programa.java
@@ -1,32 +1,104 @@
 public class Programa{
+
+	public static void sumatoria()
+	{
+		float a,b,c;
+
+		System.out.println("::  Sumatoria  ::");
+		System.out.println("Ingrese el primer numero");
+		a = ConsoleInput.getFloat();
+		System.out.println();
+		System.out.println("Ingrese el segundo numero");
+		b = ConsoleInput.getFloat();
+
+		c = a + b;
+
+		System.out.println();
+		System.out.println("El resultado es: "+c);
+		System.out.println();
+	}
+
+	public static void promedio()
+	{
+		int n;
+        float p,s=0,x;
+        System.out.println(":: Promedio ::");
+        System.out.println("Ingrese la cantidad de elementos");
+        n = ConsoleInput.getInt();
+   
+        for(int i=1;i<=n;i++)
+        { 
+         System.out.println("Ingrese el elemento "+i);
+         x = ConsoleInput.getFloat();
+         s = s + x;                
+        }   
+        p = s/n;
+        
+        System.out.println();
+        System.out.println("El promedio es: "+p);
+        System.out.println();
+	}
+
+	public static void factorial(){
+        long f=1;
+        int num;
+        System.out.println(":: Factorial ::");
+        System.out.println("Ingrese el numero");
+        num = ConsoleInput.getInt();
+        for (int i = num; i > 0; i--) {
+            f = f * i;
+        }
+        System.out.println();
+        System.out.println("El factorial de "+num+" es: "+f);
+        System.out.println();    
+    }
+
+	public static void raiznesisma()
+	{
+        double raiz;
+        double i,r;
+        System.out.println(":: Raiz n-esima ::");
+        System.out.println("Ingrese el valor del indice");
+        i = ConsoleInput.getInt();
+        System.out.println("Ingrese el valor del radicando");
+        r = ConsoleInput.getInt();
+
+        raiz = Math.pow(r,1/i);
+
+        System.out.println();
+        System.out.println("La raiz "+i+" de "+r+" es: "+raiz);
+        System.out.println();
+	}
 	public static void main(String[] args) {
-
-		/*
-		// Ejemplo lectura de datos
-
-		System.out.println("Ingrese una cadena");
-		String temp = ConsoleInput.getString();
-		System.out.println("La cadena leida fue:"+temp);
-
-		System.out.println("Ingrese un numero entero");
-		int entero = ConsoleInput.getInt();
-		System.out.println("El entero leido fue:"+entero);
-
-		System.out.println("Ingrese un numero flotante");
-		float flotante = ConsoleInput.getFloat();
-		System.out.println("El flotante leido fue:"+flotante);
-		*/
 
 		int centinela = 0;
 
 		do{
-			System.out.println("Ingrese una opcion asi:");
+			System.out.println("Elija una operacion:");
+			System.out.println();
 			System.out.println("1. Sumatoria");
-			System.out.println("2. Promedio n datos");
+			System.out.println("2. Promedio");
 			System.out.println("3. Factorial");
 			System.out.println("4. Raiz n-esima");
-			System.out.println("5. salir");
+			System.out.println("5. Salir");
 			centinela = ConsoleInput.getInt();
+
+			switch(centinela)
+			{
+				case 1: System.out.println();
+						sumatoria();
+						break;
+				case 2: System.out.println();
+						promedio();
+						break;
+				case 3: System.out.println();
+						factorial();
+						break;
+				case 4: System.out.println();
+						raiznesisma();
+						break;
+				default: System.out.println("Opcion no disponible");
+			}
 		}while(centinela!=5);
 
 	}
